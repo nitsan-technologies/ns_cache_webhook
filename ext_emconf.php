d6aae05946edd83dd06a28320366f82aa2a46892
NITSAN <info@nitsan.in>
2025-06-25 15:21:07 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
 $EM_CONF['ns_cache_webhook'] = [
-    'title' => 'Flush Webhook Cache',
-    'description' => 'Introducing our Flush Webhook Cache Extension for TYPO3 Backend, designed to streamline your web development process. With just a click, you can instantly clear the webhook cache, ensuring that your website stays updated with the latest changes and configurations.',
+    'title' => 'TYPO3 Flush Webhook Cache',
+    'description' => 'A simple TYPO3 extension to clear webhook cache directly from the backend with one click—ensuring your latest changes and configurations are always live.',
     'category' => 'plugin',
-    'author' => 'T3Planet // NITSAN',
-    'author_company' => 'T3Planet // NITSAN',
-    'author_email' => 'sanjay@nitsan.in',
+    'author' => 'Team T3Planet',
+    'author_company' => 'T3Planet',
+    'author_email' => 'info@t3planet.de',
     'state' => 'stable',
     'version' => '1.0.1',
     'constraints' => [
